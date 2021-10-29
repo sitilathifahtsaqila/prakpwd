5938b1708700b3812b38247457b43f8d0e9739a4
Siti Lathifah Tsaqila <62749859+sitilathifahtsaqila@users.noreply.github.com>
2021-10-30 02:34:39 +0700

    Add files via upload

---
+++ Kegiatan 4/table.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+
+    <title>Halaman Utama</title>
+    <?php echo '<b><h2>Data Yang Di Isi</b><br><br>' ?>
+
+</head>
+<body>
+
+    <table width='80%' border=1>
+        <?php
+        $nama =$_POST['nama'];
+        $email =$_POST['email'];
+        $website =$_POST['website'];
+        $comment =$_POST['comment'];
+        $gender =$_POST['gender'];
+
+        ?>
+
+        <tr>
+            <td><?php echo 'Nama' ?></td>
+            <td><?php echo $nama; ?></td>
+        </tr>
+        <tr>
+            <td><?php echo 'Email' ?></td>
+            <td><?php echo $email; ?></td>
+        </tr>
+        <tr>
+            <td><?php echo 'Website' ?></td>
+            <td><?php echo $website; ?></td>
+        </tr>
+        <tr>
+            <td><?php echo 'Comment' ?></td>
+            <td><?php echo $comment; ?></td>
+        </tr>
+        <tr>        
+            <td><?php echo 'Gender' ?></td>
+            <td><?php echo $gender; ?></td>
+        </tr>
+    </table>
+    <?php
+    echo "<br><a href='validasi.php'>Back</a>"
+    ?>
+</body>
+</html>
